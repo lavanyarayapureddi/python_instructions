4e1afdf123e9ed8386b297b840db7a4b97c3999c
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-02-24 19:23:00 +0530

    Create variables&datatypes.py

---
+++ variables&datatypes.py
@@ -0,0 +1,45 @@
+#variables
+a=5
+print(a)
+course="py"
+print(course)
+Course="p"
+print(Course)
+_co="sai"
+print(_co)
+_Co="see"
+print(_Co)
+#variable name cannot contain the special char in the entire variable name,and cannot start with a number in the middle of the variable name we can place the numbers
+#it is case sensitive and three are differnt cases
+age=18
+print(age)
+AGE=19
+print(AGE)
+aGe=20
+print(aGe)
+
+#datatypes
+int=9.0
+print(9)
+print(type(int))
+integer=-7
+print(integer)
+print(type(integer))
+float=99.9
+print(float)
+f=-99.9
+print(f)
+print(type(f))
+str=" "
+print(str)
+string="lav"
+print(string)
+print(type(string))
+bool=True
+print(bool)
+Bool=False
+print(Bool)
+
+
+
+
