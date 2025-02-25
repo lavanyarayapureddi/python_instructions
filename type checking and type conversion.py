7620eb28d0dfbc2d770371dad7deb02c4da57d26
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-02-25 21:22:33 +0530

    Create type checking and type conversion.py

---
+++ type checking and type conversion.py
@@ -0,0 +1,90 @@
+#checking type int 
+a=5
+print(a)
+print(type(a))
+#checking the str
+a="35"
+print(a)
+print(type(a))
+
+#checking the bool
+t=True
+print(t)
+print(type(t))
+
+#checking the float
+a=100.897
+print(a)
+print(type(a))
+
+
+#type conversion or type casting
+#int to float
+a=99
+print(a)
+print(type(a))
+b=float(a)
+print(b)
+print(type(b))
+#int to string
+a=44
+print(a)
+b=str(a)
+print(b)
+print(type(b))
+
+#int to bool
+a=66
+print(type(a))
+print(a)
+b=bool(a)
+print(type(b))
+print(b)
+
+#float to int
+a=19.99
+print(a)
+b=int(a)
+print(b)
+print(type(b))
+
+#float to string
+f=99.00
+b=str(f)
+print(b)
+print(type(b))
+
+#float to bool
+a=44.4
+print(type(a))
+b=bool(a)
+print(b)
+print(type(b))
+
+#string to int,float,bool
+s="5"
+print(type(s))
+a=int(s)
+print(a)
+print(type(a))
+s="5"
+b=float(s)
+print(b)
+print(type(b))
+c=bool(s)
+print(c)
+print(type(c))
+
+
+#bool to int,float,string
+bool=True
+a=int(bool)
+print(a)
+print(type(a))
+b=float(bool)
+print(b)
+print(type(b))
+c=str(bool)
+print(c)
+print(type(c))
+
