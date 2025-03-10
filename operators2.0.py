b1f524723259d3206094cb93e4c4f1d623737ca3
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-03-10 21:02:13 +0530

    Update operators2.0.py

--- operators2.0.py
+++ operators2.0.py
@@ -67,6 +67,33 @@ print(True & False)
 print(True | False)
 print(2 & 1)
 
+#membership operator
+#in,not in
+a=[1,2,3,4]
+print(4 in a)
+print(10 in a)
+print(4 not in a)
+print(3 not in a)
+
+#identity operator
+#is,is not
+a=10
+b=10
+print(a is b)
+
+a=10
+b=20
+print(a is not b)
+
+a=10
+b=20
+print(a is b)
+
+a=10
+b=10
+print(a is not b)
+
+
 
 
 
