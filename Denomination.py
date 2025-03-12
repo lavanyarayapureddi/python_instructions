404ab8e1b40116487f2f6f6226c8f99fdca5ec02
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-03-12 19:57:02 +0530

    Update Denomination.py

--- Denomination.py
+++ Denomination.py
@@ -1,3 +1,4 @@
+#denomination.py
 amount=int(input())
 five_hundred_notes=amount//500
 remaining_balance=amount%500
@@ -6,13 +7,18 @@ hundred_notes=remaining_balance//100
 remaining_balance=remaining_balance%100
 print("Hundred Notes (100):",hundred_notes)
 fifty_notes=remaining_balance//50
-remainin
-/
-g_balance=remaining_balance%50
+remaining_balance=remaining_balance%50
 print("Fifty Notes (50):",fifty_notes)
 ten_notes=remaining_balance//10
 remaining_balance=remaining_balance%10
 print("Ten Notes (10):",ten_notes)
 print(five_hundred_notes+hundred_notes+fifty_notes+ten_notes)
 print(amount)
-
+#output
+#1250
+#Five Hundred Notes (500):  2
+#Hundred Notes (100): 2
+#Fifty Notes (50): 1
+#Ten Notes (10): 0
+#5
+#1250
