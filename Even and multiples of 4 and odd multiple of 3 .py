2a745520e7441ef286e2d3c8e58f097766dd70c0
lavanyarayapureddi <lavanyarlvns@gmail.com>
2025-03-17 16:06:03 +0530

    Create Even and multiples of 4 and odd multiple of 3 .py

---
+++ Even and multiples of 4 and odd multiple of 3 .py
@@ -0,0 +1,12 @@
+for i in range(1,1001):
+    if i%2==0:
+        if i%4==0:
+            print(i)
+        else:
+            print("even ,but not multiple of 4")
+    else:
+        if i%3==0:
+            print(i)
+        else:
+            print("odd,but not multiple of 3")
+        
